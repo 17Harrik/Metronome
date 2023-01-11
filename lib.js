8bb92443223c16839fd0ea95e494a4712b5ec065
Kieran Harrison <17HarriK@fulford.york.sch.uk>
2023-01-11 12:38:59 +0000

    Added javascript file and completed up to Test 3

---
+++ lib.js
@@ -0,0 +1,21 @@
+// metronome
+console.log("Setting up metronome");
+const btnDecreaseBPM = document.getElementById("btnDecreaseBPM");
+const btnIncreaseBPM = document.getElementById("btnIncreaseBPM");
+const bpmInput = document.getElementById("txtBPM");
+
+var bpm = 60;   // beats per minute
+
+// add event handlers
+bpmInput.onchange = function (e) {
+
+    bpm = parseInt(bpmInput.value);
+    if (isNaN(bpm)) {
+        bpm = 50;
+    }
+    console.log(bpm)
+}
+
+btnDecreaseBPM.onclick = function (e) {
+    bpm -= 5;
+}
